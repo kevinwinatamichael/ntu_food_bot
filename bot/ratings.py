66dea7a32958fd9cf14a5851fb8adbecf1d1e7de
kevinwinatamichael <32704121+kevinwinatamichael@users.noreply.github.com>
2017-10-11 19:30:40 +0800

    Update dir data.json
    okay

--- bot/ratings.py
+++ bot/ratings.py
@@ -2,13 +2,13 @@ import json
 
 
 def get_rating(canteen, stall):
-    with open('data.json', 'r') as file:
+    with open('/app/bot/data.json', 'r') as file:
         data = json.load(file)
     return data[canteen][stall][0]
 
 
 def store_rating(canteen, stall, rating):
-    with open('data.json', 'r') as file:
+    with open('/app/bot/data.json', 'r') as file:
         data = json.load(file)
 
     acc_rating = data[canteen][stall][1]
@@ -26,7 +26,7 @@ def store_rating(canteen, stall, rating):
     data[canteen][stall][0] = avg_rating
     data[canteen][stall][1] = acc_rating
 
-    with open('data.json', 'w') as file:
+    with open('/app/bot/data.json', 'w') as file:
         json.dump(data, file)
 
 
